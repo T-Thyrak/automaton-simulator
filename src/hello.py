188a4a43e642b5655419b864e62d0c35bc3caaae
T-Thyrak <bus.teng.thaisothyrak@gmail.com>
2023-02-16 14:09:20 +0700

    showcase menu buttons

--- src/hello.py
+++ src/hello.py
@@ -2,4 +2,12 @@ from telegram import Update
 from telegram.ext import CallbackContext
 
 def hello(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("Hello, how are you!")
+    query = update.callback_query
+    query.answer()
+    
+    query.edit_message_text(text=f'Hello {query.from_user.first_name}!')
+    
+def goodbye(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text=f'Goodbye {query.from_user.first_name}!')
