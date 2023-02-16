31ab69d58ec9304e80df0cdd297814115ca8af5b
T-Thyrak <bus.teng.thaisothyrak@gmail.com>
2023-02-16 16:24:33 +0700

    allow default?

--- src/fa.py
+++ src/fa.py
@@ -2,9 +2,18 @@ from state import State
 from sym import Symbol
 
 class FA:
-    def __init__(self, states: list[State], alphabet: list[Symbol], transitions: dict[tuple[State, Symbol], State], start_state: State, final_states: list[State]):
+    def __init__(self, states: list[State] = None, alphabet: list[Symbol] = None, transitions: dict[tuple[State, Symbol], State] = None, start_state: State = None, final_states: list[State] = None):
         self.states = states
         self.alphabet = alphabet
         self.transitions = transitions
         self.start_state = start_state
         self.final_states = final_states
+        
+    def default():
+        return FA(
+            states=[],
+            alphabet=[],
+            transitions={},
+            start_state=None,
+            final_states=[],
+        )
